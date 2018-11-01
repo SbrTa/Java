36f8b2325be076ccfd9eceaa5087e0bfc1ec82e0
DESKTOP-EHV282V\SbrTa <subrataxon@gmail.com>
2018-10-31 17:53:36 -0700

    iText practice

---
+++ iText/src/main/java/com/example/demo/pdf/pdfGeneratorController.java
@@ -0,0 +1,33 @@
+package com.example.demo.pdf;
+
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.Document;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.itextpdf.text.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+@Controller
+public class pdfGeneratorController {
+
+
+    @GetMapping("/start")
+    public void pdfGenerator()throws IOException, DocumentException{
+        System.out.println("ready ... 1...... 2....... 3...... go.......");
+        String dest = "G:/others/okk.pdf";
+        com.itextpdf.text.Document pdf = new com.itextpdf.text.Document();
+        PdfWriter writer = PdfWriter.getInstance(pdf, new FileOutputStream(dest));
+        //PdfContentByte canvas = writer.getDirectContent();
+        //canvas.rectangle(new Rectangle(5,5,100,100));
+        pdf.open();
+        pdf.add(new Paragraph("hllow polapains...."));
+        pdf.close();
+
+        System.out.println("PDF generated...............");
+    }
+}
