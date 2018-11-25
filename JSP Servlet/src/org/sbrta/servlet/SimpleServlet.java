f6043da376c8ce9410e7eb8624f725cb22c44b4c
Subrata Roy <subrataxon@gmail.com>
2018-11-25 13:26:44 +0600

    ok

--- JSP Servlet/src/org/sbrta/servlet/SimpleServlet.java
+++ JSP Servlet/src/org/sbrta/servlet/SimpleServlet.java
@@ -3,16 +3,21 @@ package org.sbrta.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath" })
+@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath" },
+			initParams={@WebInitParam(name="defaultUser", value="SbrTa")}
+		)
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -21,10 +26,22 @@ public class SimpleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("hlw from servlet get method");
+		response.setContentType("text/html");
 		PrintWriter writer = response.getWriter();
-		writer.println("<h1>html h1</h1>");
+		String userName = request.getParameter("name");
+		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
+		if(userName != "" && userName!=null){
+			session.setAttribute("savedUserName", userName);
+			context.setAttribute("savedUserName", userName);
+		}
+		writer.println("Request param has username = "+userName);
+		writer.println("Session param has username = "+(String)session.getAttribute("savedUserName"));
+		writer.println("Context param has username = "+(String)context.getAttribute("savedUserName"));
+		writer.println("Init param has username = "+getServletConfig().getInitParameter("defaultUser"));
+		
+		
+		
 	}
 
 }
