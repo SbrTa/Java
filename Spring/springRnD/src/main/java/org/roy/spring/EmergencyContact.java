4030b353da450216095c227f4273a9e281a94394
Subrata Roy <subrataxon@gmail.com>
2018-11-29 20:18:49 +0600

    ok

---
+++ Spring/springRnD/src/main/java/org/roy/spring/EmergencyContact.java
@@ -0,0 +1,12 @@
+package org.roy.spring;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EmergencyContact {
+    private String name;
+    private String phoneNumber;
+}
--- Spring/springRnD/src/main/java/org/roy/spring/Patient.java
+++ Spring/springRnD/src/main/java/org/roy/spring/Patient.java
@@ -4,15 +4,14 @@ package org.roy.spring;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class Patient {
 
     private int id;
     private String name;
-    private Address address;
-
-    public void speak(){
-        System.out.println("Help !  Help !");
-    }
+//    private Address address;
+    private List<EmergencyContact> emergencyContactNames;
 }
