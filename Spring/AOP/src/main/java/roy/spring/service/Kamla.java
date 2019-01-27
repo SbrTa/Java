5a36fcf31d28382fac2309bf26b72e508294a4f0
Subrata Roy <subrataxon@gmail.com>
2019-01-27 18:43:07 +0600

    ok

--- Spring/AOP/src/main/java/roy/spring/service/Kamla.java
+++ Spring/AOP/src/main/java/roy/spring/service/Kamla.java
@@ -2,10 +2,19 @@ package roy.spring.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class Kamla {
 
     public void doIt(){
         System.out.println("Kamla is sleeping now.  Come later..");
+        Date date = new Date();
+        System.out.println(date);
+        //date.setTime(00);
+        date.setHours(0);
+        date.setMinutes(0);
+        date.setSeconds(0);
+        System.out.println(date);
     }
 }
