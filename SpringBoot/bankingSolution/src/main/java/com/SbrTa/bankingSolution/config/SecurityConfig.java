04d484b6286940f4bcede3c3c05ea56883ddc367
Subrata Roy <subrataxon@gmail.com>
2019-02-04 19:56:12 +0600

    ok

---
+++ SpringBoot/bankingSolution/src/main/java/com/SbrTa/bankingSolution/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.SbrTa.bankingSolution.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private Environment env;
+
+    private static final String SALT = "salt";
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
+
+    private static final String[] PUBLIC_MATCHES = {
+            "/webjars/**",
+            "/css/**",
+            "/js/**",
+            "/images/**",
+            "/",
+            "/about/**",
+            "/contact/**",
+            "/error/**/*",
+            "/console/**",
+            "/signup"
+    };
+}
