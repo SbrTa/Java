900830883515363f04c83ccce5e64fd5345ffcf0
Subrata Roy <subrataxon@gmail.com>
2019-02-04 12:51:58 +0600

    ok

---
+++ SpringBoot/bankingSolution/src/main/java/com/SbrTa/bankingSolution/BankingSolutionApplication.java
@@ -0,0 +1,14 @@
+package com.SbrTa.bankingSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSolutionApplication.class, args);
+	}
+
+}
+
