b3fb5e7d85a30f92a12b0ee52d6140af728a7cd9
Subrata Roy <subrataxon@gmail.com>
2019-09-25 18:13:17 +0600

    Create JsonPathConfig.java

---
+++ JsonPath/Example/JsonPathConfig.java
@@ -0,0 +1,10 @@
+//Configuration Class
+
+public class JsonPathConfig {
+    Configuration configuration = Configuration.defaultConfiguration();
+    public Configuration getConfiguration(){
+        configuration.addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL);
+        configuration.addOptions(Option.ALWAYS_RETURN_LIST);
+        return configuration;
+    }
+}
